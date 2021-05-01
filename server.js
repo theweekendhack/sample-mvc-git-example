8f0cacb87ee46bb9eb7b39bbf4a5212ac01293a5
Kadeem Best <info@theweekendhack.com>
2021-05-01 11:41:51 -0400

    API with two addtional end points

--- server.js
+++ server.js
@@ -7,6 +7,22 @@ const app  = express();
 app.get("/",(req,res)=>{
 
     res.send("<h1>It works </h1>")
+});
+
+app.get("/test",(req,res)=>{
+
+    res.json({
+        message : "Sample end Point",
+        data : ["Jon Snow", "Thanos", "Wonder WOman", "Hulk"]
+    })
+})
+
+app.get("/blah",(req,res)=>{
+
+    res.json({
+        message : "Sample end Point",
+        data : ["Jon Snow", "Thanos", "Wonder WOman", "Hulk"]
+    })
 })
 
 
